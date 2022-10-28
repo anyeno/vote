01bbe44d36792b32d2ac53732f2815b0c0d5ac63
bffw <1250458299@qq.com>
2022-10-28 16:06:42 +0800

    提交测试

--- src/main/java/com/vote/backend/Mapper/UserMapper.java
+++ src/main/java/com/vote/backend/Mapper/UserMapper.java
@@ -1,9 +1,10 @@
 package com.vote.backend.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.vote.backend.Pojo.User;
+import com.vote.backend.Model.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
+
 }
--- src/main/java/com/vote/backend/Mapper/VoteItemMapper.java
+++ src/main/java/com/vote/backend/Mapper/VoteItemMapper.java
@@ -1,7 +1,7 @@
 package com.vote.backend.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.vote.backend.Pojo.VoteItem;
+import com.vote.backend.Model.VoteItem;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
